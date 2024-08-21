6a40e6a7b9d599042668b96cca769574c77c4bfa
Yogoshiteyo <58699906+Yogoshiteyo@users.noreply.github.com>
2024-08-21 15:03:18 +0800

    Update upssh.sh

--- upssh.sh
+++ upssh.sh
@@ -45,19 +45,30 @@ echo "退出ssh，使用telnet登录到服务器"
 
 systemctl start telnet.socket
 
+# 6. 全盘查找是否存在OpenSSH源码包
+echo "正在全盘查找 openssh-9.2p1.tar.gz 源码包..."
+SEARCH_PATH=$(find / -name "openssh-9.2p1.tar.gz" 2>/dev/null | head -n 1)
 
-# 6. 下载并解压OpenSSH源码包
-mkdir -p /root/soft
-cd /root/soft
-wget https://cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-9.2p1.tar.gz
-tar xzvf openssh-9.2p1.tar.gz
+if [ -z "$SEARCH_PATH" ]; then
+    echo "未找到现有的 OpenSSH 源码包，开始下载..."
+    mkdir -p /root/soft
+    cd /root/soft
+    wget https://cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-9.2p1.tar.gz
+    SEARCH_PATH="/root/soft/openssh-9.2p1.tar.gz"
+else
+    echo "找到现有的 OpenSSH 源码包：$SEARCH_PATH"
+    cd $(dirname "$SEARCH_PATH")
+fi
 
-# 7. 安装依赖和升级OpenSSL
+# 7. 解压源码包
+tar xzvf "$SEARCH_PATH"
+
+# 8. 安装依赖和升级OpenSSL
 yum install -y gcc gcc-c++ zlib-devel pam-devel openssl-devel make vim wget
 yum install -y openssl098e.x86_64
 
-# 8. 编译安装OpenSSH
-cd /root/soft/openssh-9.2p1
+# 9. 编译安装OpenSSH
+cd openssh-9.2p1
 ./configure --prefix=/usr/local/openssh --sysconfdir=/etc/ssh --with-pam --with-zlib --with-md5-passwords --with-tcp-wrappers --with-selinux
 
 # 检查配置是否成功
@@ -68,24 +79,24 @@ else
     exit 1
 fi
 
-# 9. 修改sshd配置文件，允许root登录
+# 10. 修改sshd配置文件，允许root登录
 sed -i 's/#PermitRootLogin yes/PermitRootLogin yes/' /etc/ssh/sshd_config
 
-# 10. 配置启动脚本并修改sshd路径
-sudo cp /root/soft/openssh-9.2p1/contrib/redhat/sshd.init /etc/init.d/sshd
+# 11. 配置启动脚本并修改sshd路径
+sudo cp contrib/redhat/sshd.init /etc/init.d/sshd
 sed -i 's@SSHD=/usr/sbin/sshd@SSHD=/usr/local/openssh/sbin/sshd@' /etc/init.d/sshd
 
-# 11. 替换执行命令
+# 12. 替换执行命令
 sudo cp -arp /usr/local/openssh/bin/* /usr/bin/
 
-# 12. 重启sshd服务并配置自启
+# 13. 重启sshd服务并配置自启
 /etc/init.d/sshd restart
 chkconfig sshd on
 
-# 13. 查看OpenSSH版本
+# 14. 查看OpenSSH版本
 ssh -V
 
-# 14. 停止xinetd和telnet服务
+# 15. 停止xinetd和telnet服务
 echo "确认ssh升级成功后，手动停止telnet服务"
 echo "systemctl stop xinetd.service"
 echo "systemctl stop telnet.socket"
